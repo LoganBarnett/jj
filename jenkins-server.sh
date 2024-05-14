a479262a2561601ea269722390429de82243b667
Logan Barnett <logustus@gmail.com>
2024-05-13 18:15:03 -0700

    use proper flake eval to get the Jenkins package

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env sh
 
+# This error can be ignored:
+# java.io.IOException: No suitable implementation found: os.name=Mac OS X os.arch=aarch64 sun.arch.data.model=64
+# See: https://issues.jenkins.io/browse/JENKINS-50570
+# It is speculated that this is because /proc/meminfo doesn't exist (it doesn't
+# on macOS) _and_ top doesn't exist (it does, and I'm even using a Linux
+# version).  So maybe they both must exit.
 if which nix ; then
   listenAddress='localhost'
   listenPort=11990
   prefix='/'
+  # Get the architecture+OS "double" (Nix's term) used to identify the current
+  # system, and thus make this script platform agnostic.
+  double=$(nix eval --impure --raw --expr 'builtins.currentSystem')
+  # See https://xeiaso.net/blog/nix-flakes-look-up-package/ for how one gets
+  # packages from Nix Flakes (consider "official" documentation).
   jenkinsWarPath="$(nix eval \
-    -f '<nixpkgs>' \
-    --raw "pkgs.jenkins" \
+    --inputs-from . \
+    --raw nixpkgs#legacyPackages.$double.jenkins \
     --extra-experimental-features nix-command \
     --extra-experimental-features flakes
 )/webapps/jenkins.war"
